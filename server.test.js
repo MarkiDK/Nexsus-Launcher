bf89ed336c767b923cf2a253d650bd4ab47d5ddb
MarkiDK <45199907+MarkiDK@users.noreply.github.com>
2024-11-01 15:54:45 +0100

    server.test.js

---
+++ server.test.js
@@ -0,0 +1,8 @@
+// Opret __tests__ mappe med test filer
+// server.test.js
+describe('ServerManager', () => {
+  test('should filter servers correctly', () => {
+    const manager = new ServerManager();
+    // Add test cases
+  });
+});
